6170dab9dcb0317433996ccf5d1b372f7a546142
Thuan Luu <thuanrt123@gmail.com>
2022-07-03 21:55:09 +0700

    fix errors when building app

--- app/src/main/java/com/example/ecommerceapp/SplashScreenFragment.java
+++ app/src/main/java/com/example/ecommerceapp/SplashScreenFragment.java
@@ -14,11 +14,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SplashScreenFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SplashScreenFragment extends Fragment {
 
     private NavController navController;
